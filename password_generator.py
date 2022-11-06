6e10076e7e19a6effcb8386e3813419e2cd802ce
chabrovs <sergei2001102@gmail.com>
2022-11-06 15:07:33 +0300

    Issues fixed: same name issue, showing decripted password via cmd.

--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,13 @@
-#!/user/bin/env python3
-
 import csv
 import os
 import sys
+from typing import Dict, Any
+
 import rsa
 import base64
 import pandas as pd
 import numpy as np
 from datetime import datetime
-
 
 DESCRIPTION = {
     '': 'Password handler. Encrypts and stored password in CSV file.',
@@ -23,14 +22,17 @@ DESCRIPTION = {
 
 class DebugUtils:
     def timer(self, func_name: str):
-        "Decorator to measure time complexity"
+        """Decorator to measure functions runtime"""
+
         def outer(func):
             def wrapper(*args, **kwargs):
                 started = datetime.now()
                 result = func(*args, **kwargs)
                 print(f"{func_name} has taken: {datetime.now() - started}")
                 return result
+
             return wrapper
+
         return outer
 
 
@@ -50,10 +52,11 @@ class Generate:
         self.length = length
         self.replace = replace
         self._data_to_use = self.data['letters_lowercase'] + \
-            self.data['letters_uppercase'] + \
-            self.data['numbers'] + self.data['symbols']
+                            self.data['letters_uppercase'] + \
+                            self.data['numbers'] + self.data['symbols']
 
     def generate_password(self) -> str:
+        """Generates random password using the class settings"""
         result = np.random.choice(
             [char for char in self._data_to_use], size=self.length, replace=self.replace)
         result = "".join(result)
@@ -82,6 +85,7 @@ class Cryptography:
                 f.close()
 
             return results
+
         return wrapper
 
     @save_keys
@@ -105,8 +109,9 @@ class Cryptography:
     def decrypt(self, encrypted_message: bytes) -> str:
         return rsa.decrypt(encrypted_message, self.read_private_key())
 
+    @property
     def do_keys_exist(self) -> bool:
-        "Check if keys already exist"
+        """Check if keys already exist"""
         public_existence = os.path.exists(self._public_key_filename)
         private_existence = os.path.exists(self._private_key_filename)
         if public_existence and private_existence:
@@ -131,19 +136,45 @@ class Save_to_file(Generate, Cryptography):
         self.__cwd = os.getcwd()
 
     def is_empty(self) -> bool:
+        """Checks if passwords.csv in the directory or not"""
         if os.path.getsize(self.__cwd + '\\' + self._filename) == 0:
             return True
 
         return False
 
-    def save_records_to_csv(self, name: str, unique_name_field=False) -> None:
-        # FIXME
+    def save_records_to_csv(self, name: str, unique_name_field=True) -> None:
+        """
+        The unique_name_field setting must always be True, because python3 dictionary does not support two or more
+        keys with the same name.
+        """
+        # FIXMED
         if unique_name_field:
-            data = self.read_records_from_csv()
-            for name_field in data.items():
-                if name in name_field:
-                    name = name + 'copy_(1)'
-            name = str(name).lower()
+            @staticmethod
+            def check_if_name_exist(name: str) -> int:
+                """
+                counts how many items with the same core came already exist.
+                Core name is the name without underscores and number in it.
+                """
+                same_names = []
+                with open(self._filename, 'r', newline='') as file:
+                    reader = csv.reader(file, delimiter=',',
+                                        quotechar='|', doublequote=False, escapechar='')
+                    for row in reader:
+                        if name in row[0]:
+                            same_names.append(row)
+
+                return len(same_names)
+
+            @staticmethod
+            def rename_items_name(name: str) -> str:
+                """Adds number in parentheses ad the end of the name"""
+                same_names = check_if_name_exist(name)
+                if same_names == 0:
+                    return name
+                else:
+                    return f'{name}___{str(same_names)}'
+
+            name = rename_items_name(str(name).lower())
             value = base64.b64encode(self.encrypt(
                 self.generate_password().encode('utf8'))).decode()
             values = [name, value]
@@ -151,6 +182,7 @@ class Save_to_file(Generate, Cryptography):
                 writer = csv.writer(file)
                 if self.is_empty():
                     writer.writerow(self._field_names)
+                    writer.writerow(values)
                 else:
                     writer.writerow(values)
                 file.close()
@@ -163,12 +195,11 @@ class Save_to_file(Generate, Cryptography):
                 writer = csv.writer(file)
                 if self.is_empty():
                     writer.writerow(self._field_names)
+                    writer.writerow(values)
                 else:
                     writer.writerow(values)
 
                 file.close()
-            # raise Exception(
-                # ('Please, set the "unique_name_field" attribute of "save_records_to_csv" function to "True"'))
 
     def read_records_from_csv(self) -> dict[str, str]:
         data = {}
@@ -180,13 +211,13 @@ class Save_to_file(Generate, Cryptography):
             file.close()
         return data
 
-    def show_password(self, name) -> dict[str, str]:
+    def show_password(self, name: str) -> dict[Any, Any] | str:
         data = self.read_records_from_csv()
         for key, value in data.items():
             if key == name:
                 password = base64.b64decode(value)
                 return {name: self.decrypt(password).decode()}
-        return {name: 'Was not found!'}
+        return f"'{name}' Was not found!"
 
     def read_csv_pandas(self) -> pd.DataFrame:
         return pd.read_csv(self._filename)
@@ -204,26 +235,28 @@ class APIRequestsHandler(Save_to_file):
         super(APIRequestsHandler, self).__init__()
 
     def api_get_records_from_csv(self) -> dict[str]:
-        "Returns all records from csv file"
+        """Returns all records from csv file"""
         return self.read_records_from_csv()
 
     def api_generate_password_for(self, name: str) -> None:
-        "Generates and saves password to csv file with Name and Password columns"
+        """Generates and saves password to csv file with Name and Password columns"""
         self.save_records_to_csv(name)
         return None
 
-    def api_find_password(self, name: str) -> tuple[str]:
-        "Looks for the password. In a succeed case, return a decrypted password. (Pass name of the required query without additional args)"
+    def api_find_password(self, name: str) -> dict[Any, Any] | str:
+        """Looks for the password. In a successful case, return the decrypted password. (Pass name of the required query
+            without additional args"""
+
         return self.show_password(name.lower())
 
     def api_get_public_key(self) -> str:
-        "Return public key from 'public_key.pem' in bytes"
+        """Return public key from 'public_key.pem' in bytes | str"""
         return str(self.read_public_key())
 
     def api_generate_new_keys(self, safe=True) -> None:
-        "Generated new RSA encryption keys via safe generation method"
+        """Generated new RSA encryption keys via safe generation method"""
         if safe:
-            if self.do_keys_exist():
+            if self.do_keys_exist:
                 print("""
                     Attention!\n Keys are already exist!
                     \n Before generation new keys, make sure to save existing keys. Otherwise, you can lose access to yours' encrypted passwords!\n""")
@@ -245,7 +278,8 @@ def main():
         elif '-g' in sys.argv[1]:
             # Safe key generation
             cryptography = Cryptography()
-            if os.path.isfile(cryptography._cwd + '\\' + cryptography._public_key_filename) or os.path.isfile(cryptography._cwd + '\\' + cryptography._private_key_filename):
+            if os.path.isfile(cryptography._cwd + '\\' + cryptography._public_key_filename) or os.path.isfile(
+                    cryptography._cwd + '\\' + cryptography._private_key_filename):
                 print("""
                 Attention!\n Keys are already exist!
                 \n Before generation new keys, make sure to save existing keys. Otherwise, you can lose access to yours' encrypted passwords!\n""")
@@ -258,7 +292,7 @@ def main():
             else:
                 cryptography.generate_keys()
         elif '-f' in sys.argv[1]:
-            save_to_file.show_password(sys.argv[2].lower())
+            print(save_to_file.show_password(sys.argv[2].lower()))
         elif '-d' in sys.argv[1]:
             save_to_file.delete_row_from_csv(sys.argv[2].lower())
         elif '-pub_key' in sys.argv[1]:
